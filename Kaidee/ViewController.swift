cb9fd535bc88ab397009a2371d5cf28183401db9
Vicky <viki_vck@hotmail.com>
2017-04-05 14:22:00 +0700

    yay

--- Kaidee/ViewController.swift
+++ Kaidee/ViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slideShowView: ImageSlideshow!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        slideShowView.setImageInputs([
+            ImageSource(image: UIImage(named: "Lab6_2")!),
+            ImageSource(image: UIImage(named: "Lab6_3")!),
+            ImageSource(image: UIImage(named: "Lab6_5")!),
+            ImageSource(image: UIImage(named: "Lab6_8")!),
+            
+            ])
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func pressdsafsadfsa(_ sender: Any) {
+        print("mi")
     }
 
 
