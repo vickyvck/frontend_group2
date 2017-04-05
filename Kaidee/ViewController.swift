6b088e7377522e7be51c1d3146eb85328446e784
Vicky <viki_vck@hotmail.com>
2017-04-05 13:19:47 +0700

    Initial Commit

---
+++ Kaidee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kaidee
+//
+//  Created by Vicky on 4/5/2560 BE.
+//  Copyright © 2560 Group2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
