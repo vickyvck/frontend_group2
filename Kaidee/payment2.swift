93c26b0a3daecc582157dbcf2834acb7d8bf26ee
mink <supidsara_tha@hotmail.com>
2017-04-06 21:53:50 +0700

    hi

---
+++ Kaidee/payment2.swift
@@ -0,0 +1,35 @@
+//
+//  payment2.swift
+//  Kaidee
+//
+//  Created by supidsara thantanaporn on 4/6/17.
+//  Copyright © 2017 Group2. All rights reserved.
+//
+
+import UIKit
+
+class payment2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
