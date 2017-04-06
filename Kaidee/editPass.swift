d1ad4b153aa2587d1877f8097005c52021f5c08f
Admin <admin@Admins-MacBook-Pro-2.local>
2017-04-06 21:06:30 +0700

    setting ข้า ใครอย่าแตะ

--- Kaidee/editPass.swift
+++ Kaidee/editPass.swift
@@ -5,24 +5,23 @@
 //  Created by Admin on 4/6/2560 BE.
 //  Copyright © 2560 Group2. All rights reserved.
 //
-
 import UIKit
 
 class editPass: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     @IBOutlet var inputPassword: UITextField!
     
     
@@ -32,16 +31,15 @@ class editPass: UIViewController {
     }
     
     
-
+    
     /*
      
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
--- Kaidee/editbankaccount.swift
+++ Kaidee/editbankaccount.swift
@@ -5,17 +5,16 @@
 //  Created by Admin on 4/6/2560 BE.
 //  Copyright © 2560 Group2. All rights reserved.
 //
-
 import UIKit
 
 class editbankaccount: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -35,15 +34,14 @@ class editbankaccount: UIViewController {
         var BName = String(bankName.text!) //keep bank name
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
