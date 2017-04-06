0885cf570ecf5c0c6f89b69ee01b1af372e458ee
mink <supidsara_tha@hotmail.com>
2017-04-06 16:01:52 +0700

    hi

--- Kaidee/ProductName.swift
+++ Kaidee/ProductName.swift
@@ -1,5 +1,5 @@
 //
-//  sellerProfile.swift
+//  ProductName.swift
 //  Kaidee
 //
 //  Created by supidsara thantanaporn on 4/6/17.
@@ -8,22 +8,9 @@
 
 import UIKit
 
-class sellerProfile: UIViewController {
+class ProductName: UIViewController {
 
-    @IBOutlet weak var SellerPic: UIImageView!
-   
-    @IBOutlet weak var ScrollComment: UIView!
-    @IBOutlet weak var ShipFee: UIView!
-    @IBOutlet weak var JoinedSince: UIView!
-    @IBOutlet weak var TotalProduct: UIView!
-    @IBOutlet weak var SellerRating: CosmosView!
-    @IBAction func InfoProduct(_ sender: Any) {
-    }
-    @IBOutlet weak var SellerTel: UILabel!
-    @IBOutlet weak var SellerAddr: UILabel!
-    @IBOutlet weak var SellerName: UILabel!
     override func viewDidLoad() {
-        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
--- Kaidee/SellerProduct.swift
+++ Kaidee/SellerProduct.swift
@@ -10,6 +10,35 @@ import UIKit
 
 class SellerProduct: UIViewController {
 
+    @IBOutlet weak var SellerPic: UIImageView!
+    
+    @IBOutlet weak var SellerName: UILabel!
+    @IBOutlet weak var SellerAddress: UILabel!
+    
+    @IBOutlet weak var SellerTel: UILabel!
+    
+    @IBAction func InfoProduct(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var Product1: UIImageView!
+    
+    @IBOutlet weak var Product2: UIImageView!
+    
+    @IBOutlet weak var Product3: UIImageView!
+    
+    @IBOutlet weak var Product4: UIImageView!
+    
+    @IBOutlet weak var Product5: UIImageView!
+    
+    @IBOutlet weak var Product6: UIImageView!
+    
+    @IBOutlet weak var Product7: UIImageView!
+    
+    @IBOutlet weak var Product8: UIImageView!
+    @IBOutlet weak var ScrollProduct: UIView!
+    @IBAction func BottomBar(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
