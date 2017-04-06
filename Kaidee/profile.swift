8426684e29296c2dbfe6e2685a0b994c6c22f689
Admin <admin@Admins-MacBook-Pro-2.local>
2017-04-06 21:54:58 +0700

    ฉันทำอะไรลงไป

--- Kaidee/profile.swift
+++ Kaidee/profile.swift
@@ -9,27 +9,43 @@
 import UIKit
 
 class profile: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBOutlet var orderBox: UIScrollView!
+    @IBOutlet weak var order1: UILabel!
+    @IBOutlet weak var order2: UILabel!
+    @IBOutlet weak var order3: UILabel!
+    @IBOutlet weak var order4: UILabel!
+    
+    @IBAction func conOr1(_ sender: Any) {
     }
-    */
-
+    @IBAction func conOr2(_ sender: Any) {
+    }
+    @IBAction func conOr3(_ sender: Any) {
+    }
+    @IBAction func conOr4(_ sender: Any) {
+    }
+    
+    
+    /*
+     
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
