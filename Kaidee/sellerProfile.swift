a9be0ca150fb3e1db42d6dcefc3912dbc67fcdeb
Admin <admin@admins-macbook-pro-2.local>
2017-04-11 19:07:38 +0700

    ja die

--- Kaidee/sellerProfile.swift
+++ Kaidee/sellerProfile.swift
@@ -36,6 +36,19 @@ class sellerProfile: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBOutlet weak var menuBtn: UISegmentedControl!
+    @IBAction func menu(_ sender: Any) {
+        if (menuBtn.selectedSegmentIndex==0){
+            self.performSegue(withIdentifier: "toHome", sender: sender)
+        }else if (menuBtn.selectedSegmentIndex==1){
+            self.performSegue(withIdentifier: "toProfile", sender: sender)
+        }else if (menuBtn.selectedSegmentIndex==2){
+            self.performSegue(withIdentifier: "toWish", sender: sender)
+        }else if (menuBtn.selectedSegmentIndex==3){
+            self.performSegue(withIdentifier: "toSell", sender: sender)
+        }
+    }
+
     
 
     /*
